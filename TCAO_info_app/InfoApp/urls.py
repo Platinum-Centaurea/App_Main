94c4e5f5aae7e2572d9d3068fea562fddc7e58d7
Platinum Centaurea <sb_exvc-platinum@yahoo.co.jp>
2024-02-25 06:45:03 +0900

    Add Comit

--- TCAO_info_app/InfoApp/urls.py
+++ TCAO_info_app/InfoApp/urls.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 # views:画面遷移が主, culc:ステータス・ダメージ計算など, dataope:データ操作用
 from . import views, culc, dataope
+
+app_name = 'InfoApp'
+urlpatterns = [
+    # 起動画面
+    path('', views.main, name='main'), 
+]
--- TCAO_info_app/InfoApp/views.py
+++ TCAO_info_app/InfoApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request):
+    template_file = "InfoApp/main.html"
+    
+    options = {}
+    
+    return render(request, template_file, options)
